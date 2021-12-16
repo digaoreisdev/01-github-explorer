03fe889908a79b8d3eb0c3b100301a280d344f2a
Digão Reis <93385240+digaoreisdev@users.noreply.github.com>
2021-12-16 18:01:57 -0300

    Ignite - Aula: Source-Map

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
 export function App() {
+throw new Error ('É isso ai camarada');
+
   return  <h1>Mas que Bah!</h1>
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
+  devtool: 'eval-source-map',
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    sourceMapFilename: 'bundle.js.map',
   },
   resolve: {
     extensions: ['.js', '.jsx'],
