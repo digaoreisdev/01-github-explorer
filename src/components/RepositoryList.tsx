dcf60a27e07f3e55ce0a771d9ab013cc48ef4568
Digão Reis <93385240+digaoreisdev@users.noreply.github.com>
2021-12-21 18:18:08 -0300

    Usando TypeScript - Componentes com TypeScript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -2,12 +2,19 @@ import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
+
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
-      .then(response => response.json(0))
+      .then(response => response.json())
       .then(data => setRepositories(data))
   }, []);
 
